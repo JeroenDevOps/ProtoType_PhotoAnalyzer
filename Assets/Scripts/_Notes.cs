16c4a88872765722f367f329775121fa112cf0c2
Jeroen Brandhof <jbrandhof@hotmail.com>
2025-06-03 09:32:20 +0200

    Updated notes

--- Assets/Scripts/_Notes.cs
+++ Assets/Scripts/_Notes.cs
@@ -49,14 +49,15 @@
         i.) [x] Create photoshop file for UV map texture by using cutout/masking layer
         ii.) ...
 13. Make the photos be real screenshots with the score colors inserted into the photo
-    a.) Check how to take a screenshot in Unity (https://gamedevbeginner.com/how-to-capture-the-screen-in-unity-3-methods/)
-    b.) Open the screenshot as a Texture2D and apply the score colors to it. (Make the default colour not be black anymore, just skip)
-        i.) Probably requires rework of the controller so parts are done asyncronously 
+    a.) [x] Check how to take a screenshot in Unity (https://gamedevbeginner.com/how-to-capture-the-screen-in-unity-3-methods/)
+    b.) [x] Open the screenshot as a Texture2D and apply the score colors to it. (Make the default colour not be black anymore, just skip)
+        i.) [x] Probably requires rework of the controller so parts are done asyncronously 
             - [x] Implement screenshot in new Scene with new Controller (TestScene2 + CameraController2)
-            - Implement the photo analysis in the Controller/Scene
+            - [x] Implement the photo analysis in the Controller/Scene
 14. Create a sample scene to reflect a 'real' static scene
 15. Create a sample scene to reflect a 'real' dynamic scene 
     a). Check how to animate the scene
+        i). Do I want a real animation or behaviour (script) driven so there is some room for variation?
     b). How to tell a simple story with the scene
     c). Make things happen in different areas of the scene, so that the player has to move around to see everything and take photos of it
 16. Create a post-scene end-screen with the photos taken and a score for each photo 
